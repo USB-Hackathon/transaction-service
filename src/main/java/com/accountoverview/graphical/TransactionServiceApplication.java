da6bf85a0079805c47821ebaa0d5a63a5851107f
Mahul <mahul000@gmail.com>
2021-11-26 19:02:48 +0530

    Renaming GraphicalApplication to TransactionServiceApplication

--- src/main/java/com/accountoverview/graphical/TransactionServiceApplication.java
+++ src/main/java/com/accountoverview/graphical/TransactionServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphicalApplication {
+public class TransactionServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GraphicalApplication.class, args);
+		SpringApplication.run(TransactionServiceApplication.class, args);
 	}
 
 }

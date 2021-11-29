f0af7b80ff3d2368463afced4448df3a0d22fbcf
Mahul <mahul000@gmail.com>
2021-11-29 11:24:51 +0530

    Formatting updates

--- src/main/java/com/accountoverview/graphical/model/TransactionOverviewResponseVO.java
+++ src/main/java/com/accountoverview/graphical/model/TransactionOverviewResponseVO.java
@@ -3,7 +3,8 @@ package com.accountoverview.graphical.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TransactionOverviewResponseVO {
+public class TransactionOverviewResponseVO
+{
 	private Float totalEarnings = 0.0f;
 
 	private Float totalExpenses = 0.0f;
@@ -12,32 +13,39 @@ public class TransactionOverviewResponseVO {
 
 	private List<TransactionDetailResponseVO> transactions;
 
-	public Float getTotalEarnings() {
+	public Float getTotalEarnings()
+	{
 		return totalEarnings;
 	}
 
-	public void setTotalEarnings(Float totalEarnings) {
+	public void setTotalEarnings(Float totalEarnings)
+	{
 		this.totalEarnings = totalEarnings;
 	}
 
-	public Float getTotalExpenses() {
+	public Float getTotalExpenses()
+	{
 		return totalExpenses;
 	}
 
-	public void setTotalExpenses(Float totalExpenses) {
+	public void setTotalExpenses(Float totalExpenses)
+	{
 		this.totalExpenses = totalExpenses;
 	}
 
-	public Integer getTotalNoAccounts() {
+	public Integer getTotalNoAccounts()
+	{
 		return totalNoAccounts;
 	}
 
-	public void setTotalNoAccounts(Integer totalNoAccounts) {
+	public void setTotalNoAccounts(Integer totalNoAccounts)
+	{
 		this.totalNoAccounts = totalNoAccounts;
 	}
 
-	public List<TransactionDetailResponseVO> getTransactions() {
-		if (transactions == null)
+	public List<TransactionDetailResponseVO> getTransactions()
+	{
+		if(transactions == null)
 			transactions = new ArrayList<>();
 		return transactions;
 	}

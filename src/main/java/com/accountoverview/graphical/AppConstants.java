a6e26c86e5f09b23b4cdd5b7a8374dbde2fdbfa3
Mahul <mahul000@gmail.com>
2021-12-02 22:34:48 +0530

    Rename 'Salary' to 'Income'

--- src/main/java/com/accountoverview/graphical/AppConstants.java
+++ src/main/java/com/accountoverview/graphical/AppConstants.java
@@ -11,7 +11,7 @@ public interface AppConstants
 
 	enum TranDetail
 	{
-		Salary, Entertainment, Transfers, Bills, Groceries, Miscellaneous, Shopping
+		Income, Entertainment, Transfers, Bills, Groceries, Miscellaneous, Shopping
 
 	}
 }
